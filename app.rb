2e89ea47775c3bfae435f5d18f302af2830d6602
Mark Spangler <mspangler@financialpayments.com>
2014-09-20 22:28:07 -0500

    added yelp

--- app.rb
+++ app.rb
@@ -2,12 +2,35 @@ require 'rubygems'
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'slim'
+require 'yelp'
 
 class MyApp < Sinatra::Base
   register Sinatra::Reloader
 
+  configure do
+    enable :logging
+    set :yelp, Yelp::Client.new({ consumer_key: 'CxIgq_Qmno_4xNqoLCDngQ',
+                                  consumer_secret: 'ot8NumqSPNmseSu8m2cWC0fNUD4',
+                                  token: 'qgmf7ENPhkzFFNCgcAgMZlImrIoAto7S',
+                                  token_secret: 'BHQ01dNmKDhVZybvPK3hC_zP0Xg'
+                               })
+    set :yelp_params, {
+      term: 'restaurants',
+      limit: 5
+    }
+    set :locale, { lang: 'en '}
+  end
+
   get '/' do
     slim :index
+  end
+
+  get '/find/:longitude/:latitude' do
+    logger.info "loading data"
+    coordinates = { latitude: params[:latitude], longitude: params[:longitude] }
+    foods = settings.yelp.search_by_coordinates(coordinates, settings.yelp_params, settings.locale)
+    puts ">>> #{foods.businesses.to_json}"
+    foods.businesses.to_json
   end
 
 end
--- public/js/index.js
+++ public/js/index.js
@@ -4,8 +4,23 @@ $(document).ready(function() {
     var latlong = 'Latitude: ' + position.coords.latitude + '<br />' +
                  'Longitude: ' + position.coords.longitude;
     $('#position').html(latlong);
+    findRestaurants(position.coords.longitude, position.coords.latitude);
+  }
+
+  function findRestaurants(longitude, latitude) {
+    $.getJSON('/find/' + longitude + '/' + latitude, function(data) {
+      console.log( "success" );
+      $.each(data, function(index, restaurant) {
+        console.log(restaurant.name);
+        $('#yelp').append('<li>' + restaurant.name + ' - <a target="_blank" href="' + restaurant.mobile_url + '">' + restaurant.mobile_url + '</a></li>');
+      });
+    })
+    .fail(function() {
+      console.log( "error" );
+    });
   }
 
   getLocation(showPosition);
+
 
 });
