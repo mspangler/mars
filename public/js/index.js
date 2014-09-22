a2f07ae1a3d032e9ebbbec2d6440ae2bab81f6e7
Mark Spangler <mspangler@financialpayments.com>
2014-09-21 20:28:25 -0500

    refactoring

--- public/js/index.js
+++ public/js/index.js
@@ -27,19 +27,9 @@ $(document).ready(function() {
   }
 
   function findRestaurants(latitude, longitude) {
-    // $.getJSON('/restaurants/' + latitude  + '/' + longitude , function(data) {
-    //   $.each(data, function(index, restaurant) {
-    //     $('#restaurants').append('<li><a target="_blank" href="' + restaurant.mobile_url + '">' + restaurant.name + '</a> - ' +
-    //                              getMiles(restaurant.distance).toPrecision(2) + ' miles away</li>');
-    //   });
-    // })
-    // .fail(function() {
-    //   console.log('findRestaurants: error');
-    // });
-
     var url = '/restaurants/' + latitude  + '/' + longitude;
     React.renderComponent(
-      <RestaurantBox url={url} />,
+      <RestaurantList url={url} />,
       document.getElementById('restaurants')
     );
   }
--- public/js/restaurants.js
+++ public/js/restaurants.js
@@ -2,22 +2,33 @@
 /** @jsx React.DOM */
 
 function getMiles(meters) {
-  return meters * 0.000621371192;
+  return (meters * 0.000621371192).toPrecision(2);
 }
 
 var Restaurant = React.createClass({
   render: function() {
     return (
       <li>
-        <a target="_blank" href={this.props.restaurant.mobile_url}>{this.props.restaurant.name}</a> - {getMiles(this.props.restaurant.distance).toPrecision(2)} miles away
+        <a target="_blank" href={this.props.restaurant.mobile_url}>{this.props.restaurant.name}</a> - {getMiles(this.props.restaurant.distance)} miles away
       </li>
     );
   }
 });
 
 var RestaurantList = React.createClass({
+  loadRestaurantsFromServer: function() {
+    $.getJSON(this.props.url, function(data) {
+      this.setState({restaurants: data});
+    }.bind(this));
+  },
+  getInitialState: function() {
+    return {restaurants: []};
+  },
+  componentDidMount: function() {
+    this.loadRestaurantsFromServer();
+  },
   render: function() {
-    var restaurantNodes = this.props.restaurants.map(function(restaurant) {
+    var restaurantNodes = this.state.restaurants.map(function(restaurant) {
       return (
         <Restaurant key={restaurant.id} restaurant={restaurant} />
       );
@@ -26,22 +37,6 @@ var RestaurantList = React.createClass({
       <ul>
         {restaurantNodes}
       </ul>
-    );
-  }
-});
-
-var RestaurantBox = React.createClass({
-  getInitialState: function() {
-    return {restaurants: []};
-  },
-  componentDidMount: function() {
-    $.getJSON(this.props.url, function(data) {
-      this.setState({restaurants: data});
-    }.bind(this));
-  },
-  render: function() {
-    return (
-      <RestaurantList restaurants={this.state.restaurants} />
     );
   }
 });
