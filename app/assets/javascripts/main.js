f401c8d740b192f073ee09751e1973182c0aeaeb
Mark Spangler <mspangler@financialpayments.com>
2016-02-23 21:39:48 -0600

    work on google places

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,7 +3,7 @@ function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
     //center: { lat: -34.397, lng: 150.644 },
-    zoom: 20
+    zoom: 17
   });
 
   var infoWindow = new google.maps.InfoWindow({
@@ -45,9 +45,66 @@ function handleLocationSuccess(map, infoWindow, position) {
     position: pos,
     map: map
   });
-  
+
   marker.addListener('click', function() {
     infoWindow.open(map, marker);
+  });
+
+  // Specify location, radius and place types for your Places API search.
+  var request = {
+    location: pos,
+    radius: '500',
+    types: ['restaurant', 'bar']
+  };
+
+  // Create the PlaceService and send the request.
+  // Handle the callback with an anonymous function.
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, function(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+        // If the request succeeds, draw the place location on
+        // the map as a marker, and register an event to handle a
+        // click on the marker.
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+      }
+    }
+  });
+
+  // document.getElementById('submit').addEventListener('click', function() {
+  //   placeDetailsByPlaceId(service, map, infoWindow);
+  // });
+}
+
+function placeDetailsByPlaceId(service, map, infowindow) {
+  // Create and send the request to obtain details for a specific place,
+  // using its Place ID.
+  var request = {
+    placeId: document.getElementById('place-id').value
+  };
+
+  service.getDetails(request, function (place, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      // If the request succeeds, draw the place location on the map
+      // as a marker, and register an event to handle a click on the marker.
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+          'Place ID: ' + place.place_id + '<br>' +
+          place.formatted_address + '</div>');
+        infowindow.open(map, this);
+      });
+
+      map.panTo(place.geometry.location);
+    }
   });
 }
 
